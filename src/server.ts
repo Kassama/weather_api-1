f52147a7ea6a958e31af251c83b627b5842abb9e
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-04-26 19:51:58 +0000

    6

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,3 @@
-// import express from "express";
-// import foodRoute from "./routes/foodRoute.js";
-// import cors from "cors";
-// // We will create an express app
-// const app = express();
-
-// // The port that the express server will listen on
-// const PORT = 3000;
-
-// app.use(express.json());
-// app.use(cors());
-
-// // We define our first route
-// app.use("/api/food", foodRoute);
-
-// // Start the express server
-// app.listen(PORT, () => {
-//   console.log(`Server is listening on port ${PORT}`);
-// });
-
 import express from "express";
 import foodRoute from "./routes/foodRoute.js";
 import cors from "cors";
