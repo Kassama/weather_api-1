9636e0db00d3cf3111c48d82c4baa0f1ed0dd69e
Kassama <109762646+Kassama@users.noreply.github.com>
2024-05-02 01:35:00 +0000

    i

--- src/middleware/validators.ts
+++ src/middleware/validators.ts
@@ -15,11 +15,5 @@ export const validateFoodTypeName = param("foodType")
   .isIn(["pizza", "sushi", "burger", "salad"])
   .withMessage("foodType name must be either pizza, sushi, burger, or salad");
 
-// export const validatefoodTypeName = param("foodType")
-//   // We will use the isString method to check if the foodType param is a string
-//   .isString()
-//   // We will use the isIn method to check if the foodType param is either pizza or suzzi
-//   .isIn(["pizza", "suzzi"])
-//   // We will use the withMessage method to set a custom error message
-//   .withMessage("foodType name must be either pizza or suzzi");
+
 
